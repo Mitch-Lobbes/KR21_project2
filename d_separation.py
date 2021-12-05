58cc328308d77c31ea528aca90d702a7983a377f
malu_knrm <m1998@hotmail.de>
2021-12-05 13:42:09 +0100

    update d-speration

--- d_separation.py
+++ d_separation.py
@@ -30,6 +30,7 @@ class DSeparated:
 
         pass
 
+# keep the nodes of all three sets and their ancestors (parents, parent's parents)
     def _ancestral_graph(self) -> None:
         union_set = self.X.union(self.Y).union(self.Z)
         done = False
@@ -44,6 +45,7 @@ class DSeparated:
             if amount_vars_before == len(self.bayesNet.get_all_variables()):
                 done = True
 
+# "marry" (connect with edges) all common parents
     def _moralization(self) -> None:
         for node in self.bayesNet.get_all_variables():
             parents = self.bayesNet.structure.predecessors(node)
@@ -55,11 +57,58 @@ class DSeparated:
                     edge = (parents[i], parents[j])
                     self.bayesNet.add_edge(edge=edge)
 
+# SKIPPED => disorient graph (replace directed edges with unidirected edges)
+
+# remove givens (Z) and their edges
     def _given_removal(self) -> None:
         for node in self.Z:
             edges = self.bayesNet.get_edges_for_var(variable=node)
             self.bayesNet.del_edges(edges=edges)
             self.bayesNet.del_var(variable=node)
+
+
+# read the answer of the graph
+# variables (X,Y) disconnected => d-seperated/independent
+# varibles (X,Y) connected => dependent
+
+# way to go => check if weakly connected; if no its d-seperated
+# Question: do all nodes of X need to be connected to all nodoes of Y or is partially connected also?
+
+# make recursive function that loops through edges => always with new starting node until all edges are done or it found a connection to Y
+"""
+
+start with first node in X
+
+while not done:
+    for node in X:
+        next_node = node
+        for tuple in reversed(all_edges_list):
+            if node in tuple:
+                if node == tuple[0]:
+                    next_node = tuple[1]
+                    all_edges_list.remove(tuple)
+
+                    
+
+
+                elif node == tuple[1]:
+                    next_node = tuple[0]
+                    all_edges.remove(tuple)
+                    if tuple[2] in 
+
+
+
+
+
+
+
+    else:
+        return True (they are d-seperated)
+
+"""
+
+
+
 
 
     def prune(self) -> None:
