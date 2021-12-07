f4ba1ef610e0589f1718e5b1ce66cc6ba30f3f7e
malu_knrm <m1998@hotmail.de>
2021-12-07 17:36:19 +0100

    update

--- MAP.py
+++ MAP.py
@@ -1,5 +1,5 @@
 from BayesNet import BayesNet
-
+import pandas as pd
 
 class MAP:
 
@@ -31,5 +31,36 @@ class MAP:
                 val = var_value_true if row[node] is True else var_value_false
                 df.at[index, 'p'] *= val
 
-        # Sum up rows
         
+
+        # Sum up rows
+        for df in dfs_containing_var:
+
+            column_names = [key for key in df if key != node]
+            new_df = pd.DataFrame(columns = column_names)
+
+            for index, row in df.iterrows():
+                result_row, result_counter = find_corresponding_row(
+                    df = df,
+                    row = row,
+                    variable_tuple = (node, not row[node])
+                )
+                new_p = row['p'] + result_row['p']
+                new_df.insert(loc=index, column="p", value=new_p)
+                # delete result_row?
+                new_df.drop(df.index[[result_counter]])
+
+
+    def find_corresponding_row(self, df, row, variable_tuple):
+        row_dict  = row.to_dict()
+        del row_dict["p"]
+        result_row = df.loc[df[variable_tuple[0]] == variable_tuple[1]]
+        counter = 0
+        for key, value in row.items():
+            result_row = result_row.loc[df[key] == value]
+            row_counter = result_row.loc[df[key] == value].index[0]
+        return result_row, row_counter
+
+
+
+       
